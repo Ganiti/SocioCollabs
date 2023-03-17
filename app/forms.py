5e5558f85428c6d06d50cbba24eaa3a5fcf07279
Ganiti <ganiti.jagota@gmail.com>
2023-03-17 20:03:58 +0530

    dashboard added

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from flask_wtf          import FlaskForm
 from flask_wtf.file     import FileField, FileRequired
 from wtforms            import StringField, TextAreaField, SubmitField, PasswordField
